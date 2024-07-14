3ff2b2bbe2b58ccfda971f04ce152a10f8d5c496
ojas282 <ojaskumargupta28@gmail.com>
2024-07-14 17:33:49 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,30 @@
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+var cart_items = [];
+
+document.querySelector(".cart-itema-1").addEventListener("click", function() {
+    cart_items.push("Wallet Cards");
+    document.querySelector(".addtocart").innerHTML="Added To Cart";
+    document.querySelector("#cart-item-1-heading").innerHTML="Wallet Cards";
+    alert("Wallet Cards added to cart.");
+});
